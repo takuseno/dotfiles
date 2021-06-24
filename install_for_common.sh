ed6e7a5ddca2ed19101103a6ab7dc8fb5526f090
takuseno <takuma.seno@gmail.com>
2021-06-24 22:45:58 +0900

    Fix neovim setting

--- install_for_common.sh
+++ install_for_common.sh
@@ -3,11 +3,14 @@ cp .bashrc ~/.bashrc
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
 
+# vim setting
 cp _vimrc ~/.vimrc
 cp -r _vim ~/.vim
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
-# dein setting
+# neovim setting
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/init.vim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 rm installer.sh
