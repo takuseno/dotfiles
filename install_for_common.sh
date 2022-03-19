b18fe11889c9f30ff1fd167827d863686bcf50ac
takuseno <takuma.seno@gmail.com>
2022-03-19 09:37:43 +0900

    Copy zshrc

--- install_for_common.sh
+++ install_for_common.sh
@@ -1,4 +1,5 @@
 cp .bashrc ~/.bashrc
+cp .zshrc ~/.zshrc
 
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
