4f9e16ddd66b83fc94631d0bacb0c7c3fb4cf583
takuseno <takuma.seno@gmail.com>
2025-05-25 07:22:27 +0900

    Enable lock screen for i3wm

--- install_for_ubuntu.sh
+++ install_for_ubuntu.sh
@@ -23,7 +23,8 @@ sudo apt-get install \
     feh \
     rofi \
     xclip \
-    maim
+    maim \
+    xss-lock
 
 # install neovim
 wget https://github.com/neovim/neovim-releases/releases/download/v0.10.4/nvim-linux-x86_64.appimage
